c65b8e8e7ccea3640b03b82a95852b5ce98141e7
Phước Trần <phuoctn412@Phuocs-MacBook-Pro.local>
2023-04-12 11:58:35 +0700

    feat: edit request dayoff

--- src/app/controllers/request.controller.ts
+++ src/app/controllers/request.controller.ts
@@ -1,65 +1,56 @@
+import { Group } from '@entities/group.entity'
 import { Request as RequestDayOff } from '@entities/request.entity'
 import { RequestAppove } from '@entities/requestApprove.entity'
-import { Request, Response } from 'express'
-import dataSource from '@shared/config/data-source.config'
 import { User } from '@entities/user.entity'
-import { Repository } from 'typeorm'
-import { Group } from '@entities/group.entity'
+
+import { Request, Response } from 'express'
+import { StatusCodes } from 'http-status-codes'
 
 export class RequestDayOffController {
+  async getRequests(req: Request, res: Response) {
+    const data = await RequestDayOff.find({
+      relations: ['requestApproves'],
+    })
+    return res.status(StatusCodes.OK).json(data)
+  }
   async createDayOff(req: Request, res: Response) {
     const { from, to, reason, groupId, userId } = req.body
-
-    const result = await RequestDayOff.create({
+    const newRequestDayOff = await RequestDayOff.create({
       from,
       to,
       reason,
     }).save()
 
-    const user = await User.find({
+    const user = await User.findOne({
       where: {
         id: userId,
       },
     })
-    const group = await Group.find({
+    const group = await Group.findOne({
       where: {
         id: groupId,
       },
     })
 
-    const data = RequestAppove.create({
-      status: true,
-      user: user[0],
-      group: group[0],
-    })
-
-    return res.status(201).json(result)
-  }
-
-  async createRequestApproval(req: Request, res: Response) {
-    const user = await User.find({
+    const newRequest = await RequestDayOff.findOne({
       where: {
-        id: 1,
-      },
-    })
-    const group = await Group.find({
-      where: {
-        id: 1,
+        id: newRequestDayOff.id,
       },
     })
 
-    const data = RequestAppove.create({
-      status: true,
-      user: user[0],
-      group: group[0],
-    })
+    const data = await RequestAppove.create({
+      user: user,
+      group: group,
+      request: newRequest,
+    }).save()
 
-    return res.status(201).json(data)
+    console.log('data', data)
+
+    return res.status(201).json({ message: 'Successfully' })
   }
 
   async updateRequestApproval(req: Request, res: Response) {
-    const { requestId, from, to, reason } = req.body
-
-    res.json(req.params.id)
+    // const { requestId, from, to, reason } = req.body
+    // res.json(req.params.id)
   }
 }
---
+++ src/shared/enums/statusEnums.ts
@@ -0,0 +1,5 @@
+export enum StatusApproval {
+  ACCEPT = 'accept',
+  REJECT = 'reject',
+  PENDING = 'pending',
+}
