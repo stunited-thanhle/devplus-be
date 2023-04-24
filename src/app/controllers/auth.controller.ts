443ddacd2148ec782bd71821c1a423595b0da41a
phuoctn412 <phuoctn412@gmail.com>
2023-04-24 20:59:48 +0700

    feat: reset user password

--- src/app/controllers/auth.controller.ts
+++ src/app/controllers/auth.controller.ts
@@ -6,6 +6,8 @@ import * as ValidateHelper from '@shared/helper'
 import { Role } from '@entities/role.entity'
 import { Not } from 'typeorm'
 import { Roles } from '@shared/enums'
+import * as bcrypt from 'bcryptjs'
+
 export class AuthController {
   async login(req: Request, res: Response) {
     const { email, password } = req.body
@@ -67,5 +69,54 @@ export class AuthController {
     return res
       .status(StatusCodes.OK)
       .json({ message: 'Sucessfully', statusCode: StatusCodes.OK, roles })
+  }
+
+  async resetPassword(req: any, res: Response) {
+    const { oldPassword, newPassword, newPasswordConfirm } = req.body
+    console.log(newPassword)
+    const userId = req.user.id
+
+    const fields = ['oldPassword', 'newPassword', 'newPasswordConfirm']
+
+    const error = ValidateHelper.validate(fields, req.body)
+
+    if (error.length) {
+      const response: ErrorBody = {
+        message: error,
+        statusCode: StatusCodes.BAD_REQUEST,
+      }
+      return res.status(StatusCodes.BAD_REQUEST).json(response)
+    }
+
+    const authUser = await User.findOne({
+      where: {
+        id: userId,
+      },
+    })
+
+    if (newPasswordConfirm !== newPassword) {
+      const response: ErrorBody = {
+        message: 'Password confirm is not match',
+        statusCode: StatusCodes.BAD_REQUEST,
+      }
+      return res.status(StatusCodes.BAD_REQUEST).json(response)
+    }
+
+    if (!authUser || !(await authUser.comparePassword(oldPassword))) {
+      const responseData: ErrorBody = {
+        message: 'Password is not correct',
+        statusCode: StatusCodes.BAD_REQUEST,
+      }
+      return res.status(StatusCodes.BAD_REQUEST).json(responseData)
+    }
+
+    const passwordHash = await bcrypt.hash(newPassword, 10)
+
+    authUser.password = passwordHash
+    await authUser.save()
+
+    return res
+      .status(StatusCodes.OK)
+      .json({ message: 'Successfully', statusCode: StatusCodes.OK })
   }
 }
